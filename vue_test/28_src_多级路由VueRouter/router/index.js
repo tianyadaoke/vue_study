01039e1450f40977779ca887099126951ded47fa
Bing Zhou <bingfbi@gmail.com>
2021-07-30 13:43:05 +0200

    vue路由入门

---
+++ vue_test/28_src_多级路由VueRouter/router/index.js
@@ -0,0 +1,27 @@
+import VueRouter from "vue-router";
+import About from '../pages/About.vue'
+import Home from '../pages/Home.vue'
+import News from '../pages/News.vue'
+import Message from '../pages/Message.vue'
+export default new VueRouter({
+  routes:[
+    {
+      path:'/about',
+      component: About
+    },
+    {
+      path:'/home',
+      component: Home,
+      children:[
+        {
+          path:'news',
+          component:News
+        },
+        {
+          path:'message',
+          component:Message
+        }
+      ]
+    },
+  ]
+})
